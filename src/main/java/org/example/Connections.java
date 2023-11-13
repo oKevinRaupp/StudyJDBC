9b6e4cd38983d59632f1cce5d1c8db8126d8eaf4
devkevin <rauppke@gmail.com>
2023-11-13 19:29:56 -0300

    Study JDBC

--- src/main/java/org/example/Connections.java
+++ src/main/java/org/example/Connections.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Connections {
     public static Connection getConexao(){
         try{
+//            Properties properties = getProperties();
             String stringConexao = "jdbc:mysql://localhost:3306/curso_java?VerifyServerCertificate=false&useSSL=true";
             String usuario = "devkevin";
             String senha   = "Teste123@!";
@@ -15,5 +18,12 @@ public class Connections {
         } catch (ClassNotFoundException | SQLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        String path = "/conexao.properties";
+        properties.load(Connections.class.getResourceAsStream(path));
+        return properties;
     }
 }
