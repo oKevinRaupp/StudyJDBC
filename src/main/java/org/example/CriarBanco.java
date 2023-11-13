42b3dee968f56a061521c18286d1bdf5bde0d961
devkevin <rauppke@gmail.com>
2023-11-13 12:43:24 -0300

    Study JDBC

---
+++ src/main/java/org/example/CriarBanco.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CriarBanco {
+    public static void main(String[] args) {
+        final String stringConexao = "jdbc:mysql://localhost:3306?VerifyServerCertificate=false&useSSL=true";
+        final String usuario = "devkevin";
+        final String senha   = "Teste123@!";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(stringConexao,usuario,senha);
+
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE DATABASE curso_java");
+            System.out.println("Banco criado com sucesso!");
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String stringConexao = "jdbc:mysql://localhost:3306?VerifyServerCertificate=false&useSSL=true";
+        String usuario = "devkevin";
+        String senha   = "Teste123@!";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexao = DriverManager.getConnection(stringConexao,usuario,senha);
+            conexao.close();
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        System.out.println("Deu boa");
+    }
+}
